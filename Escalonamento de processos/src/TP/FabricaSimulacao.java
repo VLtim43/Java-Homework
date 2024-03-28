7a652703e80cb653d3ed553afd7c7262a85cbcae
VLtim43 <69370181+VLtim43@users.noreply.github.com>
2024-03-28 14:28:21 -0300

    Update FabricaSimulacao.java

--- Escalonamento de processos/src/TP/FabricaSimulacao.java
+++ Escalonamento de processos/src/TP/FabricaSimulacao.java
@@ -1,7 +1,7 @@
 package TP;
-//FERNANDO V. VALENTIM PUC-MINAS
-//Programa que simula o empacotamento dos produtos. O programa recebe o path(caminho) do arquivo txt. DEVE SER INSERIDO PELO USU¡RIO O CAMINHO QUE SE ENCONTRA O TXT
-//O Programa le o arquivo, e baseado na padronizaÁ„o do mesmo; os ordena por menor-prazo-primeiro.
+
+//Programa que simula o empacotamento dos produtos. O programa recebe o path(caminho) do arquivo txt. DEVE SER INSERIDO PELO USU√ÅRIO O CAMINHO QUE SE ENCONTRA O TXT
+//O Programa le o arquivo, e baseado na padroniza√ß√£o do mesmo; os ordena por menor-prazo-primeiro.
 
 import java.io.*;
 import java.util.Arrays;
@@ -25,22 +25,22 @@ public class FabricaSimulacao {
 
 
 	   
-		//exebiciÁ„o dos pedidos JA ORDENADOS feitos pelos braÁos mecanicos
-		//[00:00:00] = TEMPO GLOBAL. Simula o temp GERAL da f·brica(atualizado a cada CAIXA). cada braÁo mostra em qual ponto se encontra no tempo ao terminar cada caixa
+		//exebici√ß√£o dos pedidos JA ORDENADOS feitos pelos bra√ßos mecanicos
+		//[00:00:00] = TEMPO GLOBAL. Simula o temp GERAL da f√°brica(atualizado a cada CAIXA). cada bra√ßo mostra em qual ponto se encontra no tempo ao terminar cada caixa
 			
 		
 		Scanner teclado = new Scanner(System.in);
-		System.out.println("Escolha a simulaÁ„o:");
-		System.out.println("digite 1 para a simulaÁ„o com threads [!aviso! ainda em implementaÁ„o] A exibiÁ„o no terminal È bugada pois s„o executadas E exebidas ao mesmo tempo");
-		System.out.println("digite 0 para a simulaÁ„o SEM threads. O programa primeiro executa e exibe a simulaÁ„o do braÁo 1, depois do braÁo 2");
+		System.out.println("Escolha a simula√ß√£o:");
+		System.out.println("digite 1 para a simula√ß√£o com threads [!aviso! ainda em implementa√ß√£o] A exibi√ß√£o no terminal √© bugada pois s√£o executadas E exebidas ao mesmo tempo");
+		System.out.println("digite 0 para a simula√ß√£o SEM threads. O programa primeiro executa e exibe a simula√ß√£o do bra√ßo 1, depois do bra√ßo 2");
 		int opc = teclado.nextInt();
 		
 		if(opc == 1) {
 			System.out.println("PEDIDOS: " + "\r\n");
 			// MULTITHREADING
-			//AINDA EM FASE DE IMPLEMENTA«√O - PROVANDO QUE AMBOS M…TODOS EST√O FUNCIONANDO NO SISTEMA DE MULTITHREADING EM CONCORR NCIA; QUANDO EXECUTADO, A EXIBI«√O NO TERMINAL
+			//AINDA EM FASE DE IMPLEMENTA√á√ÉO - PROVANDO QUE AMBOS M√âTODOS EST√ÉO FUNCIONANDO NO SISTEMA DE MULTITHREADING EM CONCORR√äNCIA; QUANDO EXECUTADO, A EXIBI√á√ÉO NO TERMINAL
 			//FICA BUGADA POIS AS THREADS SE SOBRESSAEM UMAS NAS OUTRAS SE "ATROPELANDO" 
-			//QUANDO EXECUTADO OS DOIS M…TODOS NORMAIS, … VISIVEL QUE O JAVA EXECUTA PRIMEIRO 100% DA PRIMEIRA, DEPOIS 100% DA SEGUNDA
+			//QUANDO EXECUTADO OS DOIS M√âTODOS NORMAIS, √â VISIVEL QUE O JAVA EXECUTA PRIMEIRO 100% DA PRIMEIRA, DEPOIS 100% DA SEGUNDA
 			
 			new Thread() { 
 		        public void run() {
@@ -55,7 +55,7 @@ public class FabricaSimulacao {
 
 		} else if (opc == 0 ) {
 			System.out.println("PEDIDOS: " + "\r\n");
-			//METODOS NORMAIS. PRIMEIRO O METODO 1 … EXECUTADO; ENT√O FINALIZADO, DEPOIS O METODO 2 … EXECUTADO E FINALMENTE EXECUTADO
+			//METODOS NORMAIS. PRIMEIRO O METODO 1 √â EXECUTADO; ENT√ÉO FINALIZADO, DEPOIS O METODO 2 √â EXECUTADO E FINALMENTE EXECUTADO
 			Braco1.work(OrdenedA);
 			Braco2.work(OrdenedB);
 		}
